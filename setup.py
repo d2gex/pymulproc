779c3a065763205075a1bfa1c6011d7c755ffe59
d2gex <daniel.garcia@d2garcia.com>
2019-08-14 18:03:04 +0100

    Refactor classifiers in setup.py to remove redundant metadata

--- setup.py
+++ setup.py
@@ -27,8 +27,6 @@ setuptools.setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ]
 )
